06e9fdaffb12b11764901c1d438aea9e194cdb4c
Mostafasery <siddam.bharat@simplilearn.net>
2026-04-29 18:23:51 +0300

    Ignore ESLint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,16 @@ import globals from "globals";
 
 export default [
   {
+    files: ["**/*.js"],
     ...js.configs.recommended,
-
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "commonjs",
       globals: {
-        ...globals.node
+        ...globals.node,
+        process: "readonly"
       }
     },
-
     rules: {
       "no-console": "off",
       "no-unused-vars": "warn"
